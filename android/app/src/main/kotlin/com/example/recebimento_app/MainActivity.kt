c6e07762a141e7cf6869fa441134dcdec769e099
IanBd00 <ianbdev@hotmail.com>
2026-04-22 15:09:12 -0300

    Correção da Splash Screen

--- android/app/src/main/kotlin/com/example/recebimento_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/recebimento_app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.recebimento_app
-
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen // Importe isso
+import android.os.Bundle // Importe isso
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Isso remove a splash nativa antes de carregar o Flutter
+        installSplashScreen() 
+        super.onCreate(savedInstanceState)
+    }
+}
